d25802c0dc4bde27bbdc512e8876afce3e2597ef
Angela-GM <angelagm0112@gmail.com>
2023-08-05 17:34:00 +0200

    estilos header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./starter_code/**/*.{html,js}"],
     theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: '#540B51',
+        yellow: '#C78837',
+        blue: '#4285F4',
+      }
+    },
   },
   plugins: [],
 }
